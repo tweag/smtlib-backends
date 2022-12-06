95ed81d1f97e0764a2391b92c2955d65d49254bd
Quentin Aristote <quentin@aristote.fr>
2022-12-06 17:17:02 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  nixConfig = {
+    # Needed by callCabal2nix
+    allow-import-from-derivation = true;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    with flake-utils.lib;
+      eachSystem [system.x86_64-linux] (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hpkgs = pkgs.haskellPackages;
+        smtlib-backends = hpkgs.callCabal2nix "smtlib-backends" ./. {};
+        smtlib-backends-tests = hpkgs.callCabal2nix "smtlib-backends-tests" ./tests {inherit smtlib-backends; };
+        smtlib-backends-z3 = hpkgs.callCabal2nix "smtlib-backends-z3" ./Z3 {
+          inherit smtlib-backends smtlib-backends-tests;
+        };
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells = {
+          default = hpkgs.shellFor {
+            packages = p: [
+              smtlib-backends
+              smtlib-backends-tests
+              smtlib-backends-z3
+            ];
+
+            withHoogle = true;
+
+            buildInputs =
+              (with hpkgs; [
+                cabal-install
+                hlint
+                haskell-language-server
+              ])
+              ++ [pkgs.z3];
+
+            ## Needed by the haskell-language-server
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.z3.lib}/lib:''${LD_LIBRARY_PATH:+:}"
+            '';
+          };
+        };
+      });
+}
