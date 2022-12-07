0920dc36bd34bebf095cd847e13c480c52387863
Quentin Aristote <quentin@aristote.fr>
2022-12-07 12:30:41 +0100

    Z3: tests: test error handling

--- Z3/tests/Main.hs
+++ Z3/tests/Main.hs
@@ -1,15 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import SMTLIB.Backends
 import SMTLIB.Backends.Tests
 import qualified SMTLIB.Backends.Z3 as Z3
 import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
 main = do
   defaultMain $
     testGroup "Tests" $
-      [ testBackend "Z3" validSources noLogging $ \todo ->
-          Z3.with $ todo . Z3.toBackend
+      [ testBackend "Examples" validSources noLogging z3,
+        testBackend "Error handling" failingSources noLogging z3
       ]
   where
+    z3 todo = Z3.with $ todo . Z3.toBackend
     noLogging = const $ return ()
-    -- validSources = filter (\source -> name source `notElem` ["assertions", "unsat cores"]) sources
-    validSources = sources
+    validSources = filter (\source -> name source `notElem` ["assertions", "unsat cores"]) sources
+    failingSources =
+      [ Source "invalid command" $ \solver -> do
+          result <- command solver "this is not a valid command!!!"
+          assertBool ("Expecting error message, got: " ++ LBS.unpack result) $ "(error" `LBS.isPrefixOf` result
+      ]
