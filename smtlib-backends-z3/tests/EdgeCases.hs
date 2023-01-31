f6d77e690072d1dc8b93c0a208ecbbf156c95c23
Quentin Aristote <quentin@aristote.fr>
2023-01-31 17:08:07 +0100

    `Z3`: document edge cases in test suite

---
+++ smtlib-backends-z3/tests/EdgeCases.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module EdgeCases (edgeCases) where
+
+import Data.ByteString.Builder (Builder)
+import Data.ByteString.Lazy.Char8 as LBS
+import SMTLIB.Backends as SMT
+import qualified SMTLIB.Backends.Z3 as Z3
+import Test.Tasty
+import Test.Tasty.HUnit
+
+edgeCases :: [TestTree]
+edgeCases =
+  [ testCase "Sending an empty command" emptyCommand,
+    testCase "Sending a command expecting no response" commandNoResponse
+  ]
+
+-- | Upon processing an empty command, the backend will respond with an empty output.
+emptyCommand :: IO ()
+emptyCommand = checkEmptyResponse ""
+
+-- | Upon processing a command producing no output, the backend will respond
+-- with an empty output.
+commandNoResponse :: IO ()
+commandNoResponse = checkEmptyResponse "(set-option :print-success false)"
+
+checkEmptyResponse :: Builder -> IO ()
+checkEmptyResponse cmd = Z3.with Z3.defaultConfig $ \handle -> do
+  let backend = Z3.toBackend handle
+  response <- SMT.send backend cmd
+  assertBool ("expected empty response, got: '" <> LBS.unpack response <> "'") $ LBS.null response
